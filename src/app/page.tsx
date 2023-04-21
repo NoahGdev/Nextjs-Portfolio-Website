fd1c288d5efceb77cfbaec93722a21b9929d26b8
Noah Gomes <84874197+NoahGdev@users.noreply.github.com>
2023-04-21 22:18:33 +0100

    Updated css and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import VerticalTimeline  from '@/components/Timeline'
 import PreviousWork from '@/components/PreviousWork'
+import ContactCard from '@/components/ContactCard'
+import HireMeCard from '@/components/HireMe'
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <About/>
       <VerticalTimeline/>
       <PreviousWork/>
+      <ContactCard/>
+      <HireMeCard/>
     </>
   )
 }
