37276d4f372c7f99292d6397d95265ad59ce9fb1
Noah Gomes <84874197+NoahGdev@users.noreply.github.com>
2023-04-22 17:05:20 +0100

    hero+hireme change

--- src/components/HireMe.tsx
+++ src/components/HireMe.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const HireMeCard: React.FC = () => {
   return (
-    <div className="fixed top-12 right-0 m-8 transform hover:-translate-y-2 transition duration-500 ease-in-out blur-background hidden sm:block">
+    <div className="fixed top-12 right-0 m-8 transform hover:-translate-y-2 transition duration-500 ease-in-out blur-background hidden sm:block hover:text-black">
         <Link href="mailto:your-email@example.com" className="bg-transparent text-white border border-purple-100 hover:bg-white hover:text-black font-bold py-2 px-4 rounded-full flex items-center">
             <FaBriefcase className="text-2xl mr-2" />
             <span className="text-md text-purple-100 md:text-sm">I'm looking for a summer internship!</span>
