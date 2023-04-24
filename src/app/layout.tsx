b7f418cd67e11d34a2cd141105cbce5a070b60a3
Noah Gomes <84874197+NoahGdev@users.noreply.github.com>
2023-04-24 22:53:50 +0100

    Updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,30 @@ import Navbar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 
 export const metadata = {
-  title: 'iNoah',
-  description: 'Welcome to my portfolio!',
+  openGraph: {
+    title: 'iNoah',
+    description: 'Welcome to my portfolio!',
+    siteName: 'inoah.dev',
+    images: [
+      {
+        url: "/screenshot.png",
+        width: 800,
+        height: 600,
+      }
+    ],
+    locale: 'en-GB',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'iNoah',
+    description: 'Welcome to my portfolio!',
+    // siteId: '1467726470533754880',
+    creator: '@NoxhDevs',
+    // creatorId: '1467726470533754880',
+    images: ['/screenshot.png'],
+  },
+  category: 'technology',
 }
 
 export default function RootLayout({
